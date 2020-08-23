7da2dd6eeb391fd89e42d69cd7b2e55d42016fdb
wesleyleandro7g <wesleyleandrosantos14@gmail.com>
2020-08-22 22:04:18 -0300

    created static folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,11 @@ import CardTeam from "../components/CardTeam"
 
 import * as S from "../styles/styles"
 
+import { Descriptions, contacts, sections } from "../static"
+
 const HomePage = () => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false)
+
   const {
     background,
     LogoImage,
@@ -97,25 +101,6 @@ const HomePage = () => {
       }
     }
   `)
-
-  const Descriptions = {
-    desburocratização:
-      "Sabe a burocracia para encontrar compradores para seus minérios? Agora você pode fazer negócios sem burocracia e dor de cabeça!",
-    desregionalização:
-      "Agora você pode acessar minérios em diferentes locais do Brasil, de forma simples e prática, e escolher de acordo a viabilidade econômica.",
-    seguranca:
-      "Negociações livres de especulação, com segurança jurídica e garantias referentes à veracidade de informações fornecidas!",
-    agilidade:
-      "Te fornecemos todas as informações necessárias para que seus negócios estejam à apenas um clique de distância!",
-    historia:
-      "Somos uma startup criada por intermediários de vendas do setor mineral e desenvolvida por um time incrível! Em nossa trilha de desenvolvimento, encontramos suporte no programa de pré aceleração Agita Warm Up Serra Geral e em nosso parceiro FAVAG.",
-    visao:
-      "Pretendemos ser referência em transações de minérios rápidas, práticas e seguras em todo o mundo.",
-    tecnologia:
-      "Trabalhamos com tecnologias que estão em alta no mercado e que são utilizadas por grandes empresas!",
-  }
-
-  const [isMenuVisible, setIsMenuVisible] = useState(false)
 
   return (
     <Layout>
@@ -251,19 +236,19 @@ const HomePage = () => {
               icon={I.IoMdChatboxes}
               title="Nossa história"
               description={Descriptions.historia}
-              colorIcon="#797979"
+              colorIcon="#595959"
             />
             <CardBenefits
               icon={I.RiLightbulbFlashLine}
               title="Nossa visão"
               description={Descriptions.visao}
-              colorIcon="#797979"
+              colorIcon="#595959"
             />
             <CardBenefits
               icon={I.RiReactjsLine}
               title="Nossa tecnologia"
               description={Descriptions.tecnologia}
-              colorIcon="#797979"
+              colorIcon="#595959"
             />
           </S.BenefitsWrapper>
         </S.WrapperItems>
@@ -326,7 +311,7 @@ const HomePage = () => {
             <CardTeam
               Image={Leandro.childImageSharp.fluid}
               name="Wesley Leandro"
-              occupation="executivo de ti"
+              occupation="superintendente técnico"
               link="https://www.linkedin.com/in/wesley-leandro-43474416b"
             />
             <CardTeam
@@ -357,48 +342,5 @@ const HomePage = () => {
     </Layout>
   )
 }
-
-const sections = [
-  {
-    id: 1,
-    detail: "Página Inicial",
-    link: "#initial",
-  },
-  {
-    id: 2,
-    detail: "Vantagens",
-    link: "#benefits",
-  },
-  {
-    id: 3,
-    detail: "Sobre Nós",
-    link: "#about",
-  },
-  {
-    id: 4,
-    detail: "Parceiros",
-    link: "#parterns",
-  },
-]
-
-const contacts = [
-  {
-    id: 1,
-    detail: "seitonnegociacoes@gmail.com",
-  },
-  {
-    id: 2,
-    detail: "(38) 99169-5259",
-  },
-  {
-    id: 3,
-    detail: "(38) 99199-2007",
-  },
-  {
-    id: 4,
-    detail:
-      "Av. Tancredo de Almeida Neves, 302 - Centro, Nova Porteirinha - MG, 39525-000",
-  },
-]
 
 export default HomePage
